c5298bafd261ecf44c73b1f078bb7b5a94ca2c84
Zhichao Zhang <zzcwoshizz@163.com>
2021-07-27 16:18:15 +0800

    unuse derive

--- packages/scan/src/mappings/handleIdentity.ts
+++ packages/scan/src/mappings/handleIdentity.ts
@@ -11,15 +11,35 @@ export async function handleIdentity(method: string, event: SubstrateEvent) {
     const account = await Account.getByAddress(address);
     if (!account) return;
 
-    const identity = await api.derive.accounts.identity(address);
-    account.display = identity.display || identity.displayParent;
-    account.legal = identity.legal;
-    account.web = identity.web;
-    account.riot = identity.riot;
-    account.email = identity.email;
-    account.pgpFingerprint = identity.pgp;
-    account.image = identity.image;
-    account.twitter = identity.twitter;
+    const identity = await api.query.identity.identityOf(address);
+    const identityInfo = identity.unwrapOrDefault().info;
+    (account.additional = identityInfo.additional.isEmpty
+      ? null
+      : identityInfo.additional.toHuman().toString()),
+      (account.display = identityInfo.display.isEmpty
+        ? null
+        : identityInfo.display.asRaw.toHuman().toString()),
+      (account.legal = identityInfo.legal.isEmpty
+        ? null
+        : identityInfo.legal.asRaw.toHuman().toString()),
+      (account.web = identityInfo.web.isEmpty
+        ? null
+        : identityInfo.web.asRaw.toHuman().toString()),
+      (account.riot = identityInfo.riot.isEmpty
+        ? null
+        : identityInfo.riot.asRaw.toHuman().toString()),
+      (account.email = identityInfo.email.isEmpty
+        ? null
+        : identityInfo.email.asRaw.toHuman().toString()),
+      (account.pgpFingerprint = identityInfo.pgpFingerprint.isEmpty
+        ? null
+        : identityInfo.pgpFingerprint.unwrap().toString()),
+      (account.image = identityInfo.image.isEmpty
+        ? null
+        : identityInfo.image.asRaw.toHuman().toString()),
+      (account.twitter = identityInfo.twitter.isEmpty
+        ? null
+        : identityInfo.twitter.asRaw.toHuman().toString());
 
     account.save();
   }
