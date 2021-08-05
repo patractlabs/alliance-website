673ca8fe94766de54b67b8f9927dfd6c50d0bcdd
Zenacia <17864213831@163.com>
2021-08-05 16:15:22 +0800

    test empty url

--- packages/app/src/core/global.ts
+++ packages/app/src/core/global.ts
@@ -1,8 +1,5 @@
 console.log('process.env.REACT_APP_NODE_ENV', process.env.REACT_APP_NODE_ENV, process.env);
 export const config = {
   decimal: process.env.REACT_APP_NODE_ENV === 'development' ? 12 : 10,
-  graphqlUrl:
-    process.env.REACT_APP_NODE_ENV === 'development'
-      ? 'https://api.alliance.patract.io/graphql'
-      : 'https://api.alliance.patract.io/graphql'
+  graphqlUrl: process.env.REACT_APP_NODE_ENV === 'development' ? 'https://api.alliance.patract.io/graphql' : '-'
 };
