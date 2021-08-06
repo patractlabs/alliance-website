47eda6984eea0c39d2dc954440e6f7c22fd3db57
Zenacia <17864213831@163.com>
2021-08-06 12:40:01 +0800

    add production graphql url

--- packages/app/src/core/global.ts
+++ packages/app/src/core/global.ts
@@ -1,5 +1,8 @@
 console.log('process.env.REACT_APP_NODE_ENV', process.env.REACT_APP_NODE_ENV, process.env);
 export const config = {
   decimal: process.env.REACT_APP_NODE_ENV === 'development' ? 12 : 10,
-  graphqlUrl: process.env.REACT_APP_NODE_ENV === 'development' ? 'https://api.alliance.patract.io/graphql' : '-'
+  graphqlUrl:
+    process.env.REACT_APP_NODE_ENV === 'development'
+      ? 'https://api.alliance.patract.io/graphql'
+      : 'https://api-dot.alliance.patract.io'
 };
