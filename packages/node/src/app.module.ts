5f22a2ca9b5e92f9ad3a23e9c5e7e142b45cbbeb
Zhichao Zhang <zzcwoshizz@163.com>
2021-09-22 14:19:40 +0800

    up api version

--- packages/node/src/app.module.ts
+++ packages/node/src/app.module.ts
@@ -17,7 +17,7 @@ export class NodeOption {}
       host: process.env.DB_HOST ?? '127.0.0.1',
       port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
       username: process.env.DB_USER ?? 'postgres',
-      password: process.env.DB_PASS ?? 'postgres',
+      password: process.env.DB_PASS ?? '123456',
       database: process.env.DB_DATABASE ?? 'postgres',
     }),
     EventEmitterModule.forRoot(),
